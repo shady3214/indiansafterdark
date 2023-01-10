72de3a3e9bcf36798b057a9772d39dd57ded5161
shady3214 <122382063+shady3214@users.noreply.github.com>
2023-01-11 01:55:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,30 +23,26 @@ app.post('/interactions', verifyKeyMiddleware(process.env.public_key), async(req
 			})
 
     if (interaction.data.custom_id == `verify`) {
-        const response1 = await fetch(`https://discord.com/api/guilds/${interaction.guild_id}/members/${interaction.member.user.id}/roles/1042687123725221918`, {
-			method: "PUT",
-			headers: {
-				"Authorization": `Bot ${process.env.token}`
-			}
-		})
-	const text1 = await response1.text()
-	    console.log(text1)
+        await fetch(`https://discord.com/api/guilds/${interaction.guild_id}/members/${interaction.member.user.id}/roles/1042687123725221918`, {
+		method: "PUT",
+		headers: {
+			"Authorization": `Bot ${process.env.token}`
+		}
+	})
+	
 
-		const response2 = await fetch(`https://discord.com/api/webhooks/${interaction.application_id}/${interaction.token}/messages/@original`, {
-			method: "PATCH",
-			headers: {
-				"Authorization": `Bot ${process.env.token}`,
-				"Content-Type": "application/json"
-			},
-			body: JSON.stringify({
-				content: "Granted You Access to the Server."
-			})
+	await fetch(`https://discord.com/api/webhooks/${interaction.application_id}/${interaction.token}/messages/@original`, {
+		method: "PATCH",
+		headers: {
+			"Authorization": `Bot ${process.env.token}`,
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({
+			content: "Granted You Access to the Server."
 		})
-		
-		const text2 = await response2.text()
-		console.log(`reponse2:\n` + text2)
+	})
 			
-		res.sendStatus(200)
+	res.sendStatus(200)
     }
 })
 
